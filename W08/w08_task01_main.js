061fda88517e3128397e0aa4ad5c218f1cf94dee
WatanabeKeita1875040T <65400988+WatanabeKeita1875040T@users.noreply.github.com>
2022-05-11 14:42:32 +0900

    a

--- W08/w08_task01_main.js
+++ W08/w08_task01_main.js
@@ -1,4 +1,4 @@
-d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W08/data.csv")
+d3.csv("https://watanabekeita1875040t.github.io/InfoVis2022/W08/data.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
@@ -22,7 +22,7 @@ class BarPlot {
         this.config = {
             parent: config.parent,
             width: config.width || 256,
-            height: config.height || 256,
+            height: config.height || 128,
             margin: config.margin || {top:10, right:10, bottom:20, left:60}
         }
         this.data = data;
@@ -32,27 +32,27 @@ class BarPlot {
     init() {
         let self = this;
 
-        self.svg = d3.select('#drawing_region')
-            .attr('width', width)
-            .attr('height', height);
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
 
-        self.chart = svg.append('g')
-            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.value)])
+            .domain([0, d3.max(self.data, d => d.value)])
             .range( [0, self.inner_width] );
 
         self.yscale = d3.scaleBand()
-            .domain([0, d3.max(data, d => d.value)])
+            .domain([0, d3.max(self.data, d => d.value)])
             .range( [0, self.inner_height] )
             .paddingInner(0.1);
 
         self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(15)
+            .ticks(5)
             .tickSizeOuter(0);
 
         self.yaxis = d3.axisLeft( self.yscale )
@@ -61,7 +61,7 @@ class BarPlot {
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`)
 
-        self.xaxis_group = self.chart.append('g')
+        self.yaxis_group = self.chart.append('g')
     }
 
     update() {
@@ -88,7 +88,7 @@ class BarPlot {
              .attr("x", 0)
              .attr("y", d => yscale(d.label))
              .attr("width", d => xscale(d.value))
-             .attr("height", yscale.bandwidth());
+             .attr("height", self.yscale.bandwidth());
 
         self.xaxis_group
             .call( self.xaxis );

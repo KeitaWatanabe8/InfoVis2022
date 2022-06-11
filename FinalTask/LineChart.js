7bed19c39f2014355070eb3c2e49a75f4243e1e9
WatanabeKeita1875040T <1875040t@stu.kobe-u.ac.jp>
2022-06-12 02:15:34 +0900

    add Final task

---
+++ FinalTask/LineChart.js
@@ -0,0 +1,94 @@
+class LineChart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:30, right:30, bottom:30, left:30}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [0, self.inner_height] );
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(5);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, 0)`)
+            .attr( "id","yaxis" );
+    }
+
+    update() {
+        let self = this;
+
+        const space = 3;
+
+        self.xvalue = d => d.sepal_length;
+        self.yvalue = d => d.sepal_width;
+
+        const xmin = d3.min( self.data, self.xvalue ) - space;
+        const xmax = d3.max( self.data, self.xvalue ) + space;
+        self.xscale.domain( [xmin, xmax] );
+
+        const ymin = d3.min( self.data, self.xvalue ) - space;
+        const ymax = d3.max( self.data, self.xvalue ) + space;
+        self.yscale.domain( [ymax, ymin] );
+
+        self.line = d3.line()
+            .x( d => self.xscale( self.xvalue(d) ) )
+            .y( d => self.yscale( self.yvalue(d) ) );
+
+        self.area = d3.area()
+            .x( d => self.xscale( self.xvalue(d) ) )
+            .y1( d => self.yscale( self.yvalue(d) ) )
+            .y0( self.inner_height );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.append('path')
+            .attr('d', self.line(self.data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
+
+        self.chart.append('path')
+            .attr('d', self.area(self.data))
+            .attr('stroke', 'black')
+            .attr('fill', 'pink');
+
+        self.xaxis_group
+            .call( self.xaxis );
+
+        self.yaxis_group
+            .call( self.yaxis );
+    }
+}
---
+++ FinalTask/main.js
@@ -0,0 +1,77 @@
+let input_data;
+let scatter_plot;
+let scatter_plot2;
+let scatter_plot3;
+let line_chart;
+let filter = [];
+
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W12/iris.csv")
+    .then( data => {
+        input_data = data;
+        input_data.forEach( d => {
+            d.sepal_length = +d.sepal_length;
+            d.sepal_width = +d.sepal_width;
+        });
+
+        const color_scale = d3.scaleOrdinal( d3.schemeCategory10 );
+        color_scale.domain(['setosa','versicolor','virginica']);
+
+        scatter_plot = new ScatterPlot( {
+            parent: '#drawing_region_scatterplot',
+            width: 128,
+            height: 128,
+            margin: {top:10, right:10, bottom:50, left:50},
+            xlabel: 'Sepal length [cm]',
+            ylabel: 'Sepal width [cm]',
+            cscale: color_scale
+        }, input_data );
+        console.log( input_data );
+        scatter_plot.update();
+
+        scatter_plot2 = new ScatterPlot( {
+            parent: '#drawing_region_scatterplot2',
+            width: 128,
+            height: 128,
+            margin: {top:10, right:10, bottom:50, left:50},
+            xlabel: 'Sepal length [cm]',
+            ylabel: 'Sepal width [cm]',
+            cscale: color_scale
+        }, input_data );
+        console.log( input_data );
+        scatter_plot2.update2();
+
+        scatter_plot3 = new ScatterPlot( {
+            parent: '#drawing_region_scatterplot3',
+            width: 128,
+            height: 128,
+            margin: {top:10, right:10, bottom:50, left:50},
+            xlabel: 'Sepal length [cm]',
+            ylabel: 'Sepal width [cm]',
+            cscale: color_scale
+        }, input_data );
+        console.log( input_data );
+        scatter_plot3.update2();
+
+        line_chart = new LineChart( {
+            parent: '#drawing_region_linechart',
+            width: 256,
+            height: 256,
+            margin: {top:10, right:10, bottom:50, left:50},
+            xlabel: 'Species',
+            cscale: color_scale
+        }, input_data );
+        line_chart.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+function Filter() {
+    if ( filter.length == 0 ) {
+        scatter_plot.data = input_data;
+    }
+    else {
+        scatter_plot.data = input_data.filter( d => filter.includes( d.species ) );
+    }
+    scatter_plot.update();
+}
